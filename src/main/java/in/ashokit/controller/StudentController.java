9ce1c355f57f1c92905f0f7e34e819983df5c78a
Suhasini Shinde <suha@1810>
2024-03-09 22:19:38 +0530

    first commit

---
+++ src/main/java/in/ashokit/controller/StudentController.java
@@ -0,0 +1,80 @@
+package in.ashokit.controller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import in.ashokit.binding.Student;
+import in.ashokit.entity.StudentEntity;
+import in.ashokit.repository.StudentRepository;
+
+@Controller
+public class StudentController {
+	
+	
+	@Autowired
+	private StudentRepository repo;
+	
+	
+	//method to load the form
+			@GetMapping("/")
+			public String loadForm(Model model) {
+				loadFormData(model);
+				
+				return "index";
+				
+			}
+			
+			
+	private void loadFormData(Model model) {
+		List<String> coursesList =new ArrayList<>();
+		coursesList.add("Java");
+		coursesList.add("DevOps");
+		coursesList.add("AWS");
+		coursesList.add("Python");
+		
+		List<String> timingsList=new ArrayList<>();
+		timingsList.add("Morning");
+		timingsList.add("Afternoon");
+		timingsList.add("Evening");
+
+		
+		Student student =new Student();
+		model.addAttribute("courses", coursesList);
+		model.addAttribute("timings", timingsList);
+		model.addAttribute("student",student);
+	}
+	
+	//method to save student Data
+	
+	//method to display saved data
+	
+		
+		@PostMapping("/save")
+		public String handleSubmit(Student s, Model model) {
+			
+			//logic to save
+			model.addAttribute("msg", "Student Saved");
+			
+			StudentEntity entity=new StudentEntity();
+			
+			//Copy student data from binding obj to entity obj
+			BeanUtils.copyProperties(s, entity);
+			
+			entity.setTimings(Arrays.toString(s.getTimings()));
+			
+			repo.save(entity);
+			
+			loadFormData(model);
+			
+			return "index";
+			
+		}
+}
